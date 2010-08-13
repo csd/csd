094ece85b19043ab8a9115ef37a90b96c1208943
AIXi <TTAAI@kth.se>
2010-08-13 17:10:31 +0800

    Added i2conf skelleton

---
+++ lib/csd/application/i2conf.rb
@@ -0,0 +1,27 @@
+# -*- encoding: UTF-8 -*-
+require 'csd/application/default/base'
+require 'csd/application/i2conf/base'
+
+module CSD
+  module Application
+    # This is the Application Module for i2conf, a sip video conference MCU.
+    #
+    module i2conf
+      class << self
+
+        include CSD::Application::Default
+
+        # This method will check which system we're on and initialize the correct sub-module
+        #
+        def instance
+          if Gem::Platform.local.ubuntu?
+            Base.new
+          else
+            raise 'Sorry, currently only Ubuntu is supported.'
+          end
+        end
+
+      end
+    end
+  end
+end
---
+++ lib/csd/application/i2conf/base.rb
@@ -0,0 +1,62 @@
+# -*- encoding: UTF-8 -*-
+require 'csd/application/default/base'
+require 'csd/application/minisip/base'
+
+module CSD
+  module Application
+    module I2conf
+      class Base < CSD::Application::Base
+        
+        include ::CSD::Application::Minisip
+        
+        # A list of apt-get packages that are required to install i2conf.
+        #
+        DEBIAN_DEPENDENCIES = %w{ libboost-dev libboost-thread-dev liblog4cxx* }
+        
+        def install
+          @minisip = ::CSD::Application::Minisip::Base.new
+          define_relative_paths
+          UI.separator
+          UI.info "This operation will download and install the i2conf SIP video conferencing MCU.".green.bold
+          UI.separator
+          introduction
+          install!
+        end
+        
+        def install!
+          #create_working_directory
+          #
+
+          send_notification
+        end
+        
+        def introduction
+          UI.info " Working directory:       ".green.bold + Path.work.to_s.yellow
+          if Options.debug
+            UI.info " Your Platform:           ".green + Gem::Platform.local.humanize.to_s.yellow
+            UI.info(" Application module:      ".green + self.class.name.to_s.yellow)
+          end
+          UI.separator
+          if Options.help
+            UI.info Options.helptext
+            # Cleanup in case the working directory was temporary and is empty
+            Path.work.rmdir if Options.temp and Path.work.directory? and Path.work.children.empty?
+            raise CSD::Error::Argument::HelpWasRequested
+          else
+            raise Interrupt unless Options.yes or Options.reveal or UI.continue?
+          end
+        end
+        
+        
+        def send_notification
+          Cmd.run %{notify-send --icon=gdm-setup "I2conf installation complete" "You are now ready to use your SIP MCU." }, :internal => true, :die_on_failure => false
+        end
+        
+        def define_relative_paths
+          Path.packages          = Pathname.new(File.join(Path.work, decklink_basename))
+        end
+        
+      end
+    end
+  end
+end
