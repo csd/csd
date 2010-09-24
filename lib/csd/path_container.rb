ac44df8debbe5b7ff41de99912d5727794e3109d
AIXi <TTAAI@kth.se>
2010-09-24 17:06:59 +0200

    Added rdcoc for Minisip core module

--- lib/csd/path_container.rb
+++ lib/csd/path_container.rb
@@ -4,18 +4,26 @@ require 'ostruct'
 module CSD
   class PathContainer < OpenStruct
     
+    # This method chooses and holds the container for root privilege.
+    #
     def root
       @root ||= Dir.pwd
     end
     
+    # This method chooses and holds the container for gem.
+    #
     def gem
       @gem ||= File.expand_path(File.join(File.dirname(__FILE__), '..' ,'..'))
     end
     
+    # This method chooses and holds the container for vendor.
+    #
     def vendor
       @vendor ||= File.join(self.gem, 'vendor')
     end
     
+    # This method chooses and holds the container for applications.
+    #
     def applications
       @applications ||= File.expand_path(File.join(self.gem, 'lib', 'csd', 'application'))
     end
